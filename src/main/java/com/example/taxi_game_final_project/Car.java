09af77cbe0a4cf7ec3e0491352d7cf947027374c
Noah Spiegelman <noahspiegelman05@gmail.com>
2023-11-30 10:05:28 -0800

    Initial Commit

---
+++ src/main/java/com/example/taxi_game_final_project/Car.java
@@ -0,0 +1,4 @@
+package com.example.taxi_game_final_project;
+
+public class Car {
+}
---
+++ src/main/java/com/example/taxi_game_final_project/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.taxi_game_final_project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
