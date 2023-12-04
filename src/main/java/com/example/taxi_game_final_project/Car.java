fddcfa1e71e2201eed01f27a7c5df726fb1040c6
Noah Spiegelman <noahspiegelman05@gmail.com>
2023-12-04 12:33:47 -0800

    Started Work on Map with Scene Builder

--- src/main/java/com/example/taxi_game_final_project/Car.java
+++ src/main/java/com/example/taxi_game_final_project/Car.java
@@ -1,5 +1,13 @@
 package com.example.taxi_game_final_project;
 
 public class Car {
+    final int xVelocity = 5;
+    final int yVelocity = 5;
+    int xStart;
+    int yStart;
 
+
+//    Car(int ){
+//
+//    }
 }
---
+++ src/main/java/com/example/taxi_game_final_project/Location.java
@@ -0,0 +1,5 @@
+package com.example.taxi_game_final_project;
+import javafx.scene.shape.Rectangle;
+
+public class Location extends Rectangle{
+}
